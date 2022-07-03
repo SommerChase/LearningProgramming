148cf4cc87077930799282a15e23367b4816f9f9
Chase Sommer <robertchasesommer@gmail.com>
2022-07-02 22:21:06 -0400

    Started CPP in Codecademy

---
+++ Codecademy/Cpp/currency.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  double pesos;
+  double reais;
+  double soles;
+  double dollars;
+
+  std::cout << "Enter number of Colombian Pesos: \n";
+  std::cin >> pesos;
+  std::cout << "You entered " << pesos << " Pesos";
+  
+  return 0;
+}
---
+++ SICP/Test.rkt
@@ -0,0 +1,4 @@
+#lang sicp
+
+(define (plural wd)
+  (word ws `s))
