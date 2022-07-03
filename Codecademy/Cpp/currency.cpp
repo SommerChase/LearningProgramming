89e0046f1e7ed86ce15fb95ef947bc93c824ed62
Chase Sommer <robertchasesommer@gmail.com>
2022-07-03 15:41:35 -0400

    Finished basic syntax in C++

--- Codecademy/Cpp/currency.cpp
+++ Codecademy/Cpp/currency.cpp
@@ -8,7 +8,18 @@ int main() {
 
   std::cout << "Enter number of Colombian Pesos: \n";
   std::cin >> pesos;
-  std::cout << "You entered " << pesos << " Pesos";
+  std::cout << "You entered " << pesos << " Pesos \n";
+
+  std::cout << "Enter number of Reais: \n";
+  std::cin >> reais;
+  std::cout << "You entered " << reais << " Reais \n";
+
+  std::cout << "Enter number of Soles: \n";
+  std::cin >> soles;
+  std::cout << "You entered " << soles << " Soles \n";
   
+  dollars = pesos * .87 + reais * .79 + soles * .23;
+  std::cout << "You have " << dollars << " USD!";
+
   return 0;
 }
