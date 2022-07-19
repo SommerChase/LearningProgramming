a7fa262f1b714208ece49ad904d30ec76e1ee975
Chase Sommer <robertchasesommer@gmail.com>
2022-07-19 08:34:59 -0400

    Solution 2 to Lists challenge 3 needs BinarySearch

---
+++ Educativeio/PythonForProgrammers/DataStructuresAndAlgorithms/BinaryTrees/TraversalAlgorithms.py
@@ -0,0 +1,16 @@
+"""
+    There are three common ways to traverse a tree in depth-first order:
+
+    1. In-order
+    2. Pre-order
+    3. Post-order
+    Let’s begin with the Pre-order Traversal.
+
+    Pre-order Traversal-----------------------------------------------
+    
+    Here is the algorithm for a pre-order traversal:
+    1. Check if the current node is empty/null.
+    2. Display the data part of the root (or current node).
+    3. Traverse the left subtree by recursively calling the pre-order method.
+    4. Traverse the right subtree by recursively calling the pre-order method.
+    """
