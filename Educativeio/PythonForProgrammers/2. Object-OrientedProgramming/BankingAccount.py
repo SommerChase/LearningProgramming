2e3b824e0770bf51540a15002cd6897db02764e2
Chase Sommer <robertchasesommer@gmail.com>
2022-07-30 10:08:11 -0400

    changes from last time?

---
+++ Educativeio/PythonForProgrammers/2. Object-OrientedProgramming/BankingAccount.py
@@ -0,0 +1,12 @@
+class Account:
+    def __init__(self, title=None, balance=0):
+        self.title = title
+        self.balance = balance
+
+
+class SavingsAccount(Account):
+    def __init__(self, title=None, balance=0, interestRate=0):
+        super().__init__(title, balance)
+        self.interestRate = interestRate
+
+Account("Mark", 5000, 5)
---
+++ Educativeio/PythonForProgrammers/2. Object-OrientedProgramming/FullStudent.py
@@ -0,0 +1,15 @@
+class Student:
+    __name = None
+    __rollNumber = None
+
+    def getName(self):
+        return self.__name
+
+    def setName(self, name):
+        self.__name = name
+
+    def getRollNumber(self):
+        return self.__rollNumber
+
+    def setRollNumber(self, rollNumber):
+        self.__rollNumber = rollNumber
