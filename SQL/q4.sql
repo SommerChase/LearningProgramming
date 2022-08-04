dd02a8d7ddec1ebe171ebd1a9fef4e5f8d0531f1
Chase Sommer <robertchasesommer@gmail.com>
2022-08-04 11:23:17 -0400

    removed old

--- SQL/q4.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  DISTINCT UPPER(category_name) AS category_name_upper
-FROM
-  bigquery-public-data.iowa_liquor_sales.sales
-WHERE
-  UPPER(category_name) LIKE '%WHISKIES' 
-  AND UPPER(category_name) NOT LIKE '%RYE%'
-  AND UPPER(category_name) NOT LIKE '%BOURBON%'
-  AND UPPER(category_name) NOT LIKE '%SCOTCH%'
